ff11d06792d08ff4d194d537648c614e1de01d4a
Freddy Reza <rezafreddy@gmail.com>
2022-03-07 16:14:16 -0600

    Loop Image Arra,  Wrap Images, Data On Divs

--- client.js
+++ client.js
@@ -1,1 +1,23 @@
 console.log('Here are all the available people:', people);
+
+$(ready);
+let counter = 1;
+function ready() {
+for (let person of people) {
+
+    let row = $(`
+    <div>
+        <img src="https://github.com/${person.githubUsername}.png?size=250" alt="Profile image of ${person.name}">
+    </div>
+    `)
+    row.data(`name`, counter);
+    counter++;
+    $('#container').append(row);
+    console.log(row.data('name'));
+    
+    } 
+
+
+}
+
+    
