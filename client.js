57d02b41992cbd150626ff43861a0a54b1778884
Freddy Reza <rezafreddy@gmail.com>
2022-03-07 16:37:06 -0600

    added faceSeclector function, random number generator.

--- client.js
+++ client.js
@@ -3,21 +3,68 @@ console.log('Here are all the available people:', people);
 $(ready);
 let counter = 1;
 function ready() {
-for (let person of people) {
+    for (let person of people) {
 
-    let row = $(`
-    <div>
+        let row = $(`
+    <div class="faces">
         <img src="https://github.com/${person.githubUsername}.png?size=250" alt="Profile image of ${person.name}">
     </div>
     `)
-    row.data(`name`, counter);
-    counter++;
-    $('#container').append(row);
-    console.log(row.data('name'));
-    
-    } 
+        row.data(`name`, counter);
+        counter++;
+        $('#container').append(row);
+        console.log(row.data('name'));
 
+    }
+
+    randomNumber(1,7);
+
+    faceSelector(randomNumber(1,7))
+
+    $('.faces').on('click', imageClicked);
+}
+
+function imageClicked() {
+    console.log('click');
+    let faceNumber= $(this).data(`name`);
+
+    console.log( faceNumber );
 
 }
 
-    
+function randomNumber(min, max){
+    return Math.floor(Math.random() * (1 + max - min) + min);
+}
+
+function faceSelector( numb ){
+
+        switch ( numb ) {
+            case 1:
+                $(`h2`).text(`Find Dev`)
+                break;
+            case 2:
+                $(`h2`).text(`Find Dane`)
+                break;
+            case 3:
+                $(`h2`).text(`Find Matt`)
+                break;
+            case 4:
+                $(`h2`).text(`Find Kris`)
+                break;
+            case 5:
+                $(`h2`).text(`Find Edan`)
+                break;
+            case 6:
+                $(`h2`).text(`Find Liz`)
+                break;
+            case 7:
+                $(`h2`).text(`Find Chris`)
+                break;
+            
+        
+            default:
+                break;
+        
+    }
+}
+
